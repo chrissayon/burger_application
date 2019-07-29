f57032ddba0f565e6ed5d2dd61516b124ce8472f
unknown <Christopher.L.Sayon@gmail.com>
2019-07-29 23:56:07 +1000

    Layout Component Placeholder, testing TravisCI

---
+++ src/hoc/auxCompoennt.js
@@ -0,0 +1,7 @@
+const aux = (props) => props.children;
+
+export default aux;
+
+//Used for wrapping adjacent components into one component
+//Hence why props is outputted
+//Having Aux.js as a name breaks the code
