cea7478e73a37f85ac2c2589ed58a9595bc21ddc
unknown <Christopher.L.Sayon@gmail.com>
2019-07-30 22:40:58 +1000

    Burger Components Made

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import classes from './Layout.module.css'
+
 const Layout = (props) => (
     <React.Fragment>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
+        <div>Build Controls</div>
     </React.Fragment>
 );
 
