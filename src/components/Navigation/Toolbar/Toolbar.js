1827b7c4341422167a75461b2693adb49e657ce5
unknown <Christopher.L.Sayon@gmail.com>
2019-07-31 22:51:16 +1000

    Added highlightable component too toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => ( //<Header> is normally for beginnign of navigation components
     <header className={classes.Toolbar}> 
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
