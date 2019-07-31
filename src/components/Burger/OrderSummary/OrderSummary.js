aff31835fc085ab836fc1ab962dc11a788146588
unknown <Christopher.L.Sayon@gmail.com>
2019-08-01 00:25:48 +1000

    Improved performance by stopping rendering on show

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span>{igKey}</span>: {props.ingredients[igKey]}
+                    <span>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>
             )
         });
-    
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            {ingredientSummary}
-            <p><strong>Total Price: {props.totalPrice.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button 
-                btnType="Danger"
-                clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button 
-                btnType="Success"
-                clicked={props.purchaseContinued}>CONTINUE</Button>
-        </React.Fragment>
-    )
+
+        return (
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                {ingredientSummary}
+                <p><strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button 
+                    btnType="Danger"
+                    clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button 
+                    btnType="Success"
+                    clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </React.Fragment>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
