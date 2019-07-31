dc32d2c132f471e0a68eb295b2ec5418abaabd21
unknown <Christopher.L.Sayon@gmail.com>
2019-07-31 21:00:29 +1000

    Ordering summary modal can be cancelled

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,6 +30,10 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: true})
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
     //Update purchase
     updatePurchaseState (ingredients) {
 
@@ -109,7 +113,10 @@ class BurgerBuilder extends Component{
 
         return (
             <React.Fragment>
-                <Modal show={this.state.purchasing}>
+                <Modal 
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}
+                    >
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />        
