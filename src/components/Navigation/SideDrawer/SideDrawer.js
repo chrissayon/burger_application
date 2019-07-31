38a9366884d77bc7530063ed4e5b4a995fc5db62
unknown <Christopher.L.Sayon@gmail.com>
2019-07-31 23:48:43 +1000

    Implement backdrop for mobile

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
     //Conditionally attach classes so there will be animation
+    
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo height="11%"/>
+        <React.Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </React.Fragment>
     );
 }
 
